703888709d5487d75290bf18b538b2c3c0d8cbbf
Joao <joao.khalaf@gmail.com>
2025-02-03 22:54:06 -0300

    add dashboard, sign-in, and sign-up pages with Clerk integration

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import { UserButton } from '@clerk/nextjs'
+import React from 'react'
+
+function page() {
+  return (
+    <>
+   <h1>oi</h1>
+    </>
+  )
+}
+
+export default page
---
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,12 @@
+import { SignIn } from "@clerk/nextjs";
+
+export default function Page() {
+  return (
+      <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+         <div className="w-full max-w-md p-8 bg-white rounded shadow-md">
+           <h1 className="mb-6 text-2xl font-bold text-center">Sign Up</h1>
+           <SignIn />
+         </div>
+       </div>
+  );
+}
